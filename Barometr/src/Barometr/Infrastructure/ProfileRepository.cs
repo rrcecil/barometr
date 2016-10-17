f881e5a41d61b2c184abb07d1e431bac8b9127f9
Dipsi Bhattarai <dipsikha4@gmail.com>
2016-10-17 14:48:25 -0500

    ProfileRepository added

---
+++ Barometr/src/Barometr/Infrastructure/ProfileRepository.cs
@@ -0,0 +1,27 @@
+﻿using Barometr.Data;
+using Barometr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Barometr.Infrastructure
+{
+    public class ProfileRepository : GenericRepository<Profile>
+    {
+        public ProfileRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public IQueryable<Profile> GetProfiles()
+        {
+            return _db.Profiles;
+        }
+
+
+        public Profile GetProfileById(int id)
+        {
+            return _db.Profiles.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
