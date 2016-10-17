3a7a9b2b1828236406639738609cf9d69996ab91
Dipsi Bhattarai <dipsikha4@gmail.com>
2016-10-17 14:39:25 -0500

    Reviewrepository created

---
+++ Barometr/src/Barometr/Infrastructure/ReviewRepository.cs
@@ -0,0 +1,27 @@
+﻿using Barometr.Data;
+using Barometr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Barometr.Infrastructure
+{
+    public class ReviewRepository : GenericRepository<Review>
+    {
+        public ReviewRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public IQueryable<Review> GetReviews()
+        {
+            return _db.Reviews;
+        }
+
+
+        public Review GetReviewById(int id)
+        {
+            return _db.Reviews.FirstOrDefault(r => r.Id == id);
+        }
+    }
+}
