8d5cfaba0b6d74bf4ffc5f2ea5f78f1aad89ebfb
Matthew Lemons <lemons3312@gmail.com>
2016-10-19 15:50:20 -0500

    added star ratings to the bar profiles

--- Barometr/src/Barometr/Controllers/BarsController.cs
+++ Barometr/src/Barometr/Controllers/BarsController.cs
@@ -38,7 +38,7 @@ namespace Barometr.Controllers
         [HttpPost]
         public void Post([FromBody]BarDTO value)
         {
-            _service.AddBar(value);
+            //_service.AddBar(value);
         }
 
         // PUT api/values/5
