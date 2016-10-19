e0ce27a3f8b13b6401a3929252ef9d6978b13c7b
Jon Brogdon <jon.brogdon@gmail.com>
2016-10-19 13:12:35 -0500

    Created search page to search google places for bars

--- Barometr/src/Barometr/wwwroot/ngApp/controllers/SearchController.ts
+++ Barometr/src/Barometr/wwwroot/ngApp/controllers/SearchController.ts
@@ -5,10 +5,29 @@ namespace Barometr.Controllers {
         public result;
         public message = 'Hello from the about page!';
         public options;
+        public neighborhoods;
+        public bars;
+
         constructor(public ngGPlacesAPI) {
             this.options = {
                 types: ['establishment']
             };
+            this.neighborhoods = {
+                heights: { latitude: 29.790128, longitude: -95.402796 },
+                riceVillage: { latitude: 29.715253, longitude: -95.418873 },
+                pearland: { latitude: 29.543877, longitude: -95.387624 }
+            };
+
+            
+        }
+
+        public getBars(hood) {
+            var hoodObj = JSON.parse(hood);
+
+            this.ngGPlacesAPI.nearbySearch({ latitude: hoodObj.latitude, longitude: hoodObj.longitude }).then((res) => {
+                this.bars = "";
+                this.bars = res;
+            });
         }
 
         public logThis() {
