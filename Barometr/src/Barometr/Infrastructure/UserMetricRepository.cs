6d072e5698f9a9d532b5ee5f190c43b7214a5a02
Dipsi Bhattarai <dipsikha4@gmail.com>
2016-10-19 16:38:14 -0500

    RandomBar

---
+++ Barometr/src/Barometr/Infrastructure/UserMetricRepository.cs
@@ -0,0 +1,23 @@
+﻿using Barometr.Data;
+using Barometr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Barometr.Infrastructure
+{
+    public class UserMetricRepository : GenericRepository<ApplicationUser>
+    {
+        public UserMetricRepository(ApplicationDbContext db) : base(db)
+        {
+    }
+            public IQueryable<ApplicationUser> GetUsers()
+        {
+            return _db.Users;
+        }
+
+        
+       
+    }
+}
