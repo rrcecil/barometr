190b697c4f2e29bbd88269c3654de5dcc19a052d
Jon Brogdon <jon.brogdon@gmail.com>
2016-10-31 11:33:27 -0500

    add business hours model. add service and repo

---
+++ Barometr/src/Barometr/Infrastructure/BusinessHoursRepository.cs
@@ -0,0 +1,22 @@
+﻿using Barometr.Data;
+using Barometr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Barometr.Infrastructure
+{
+    public class BusinessHoursRepository : GenericRepository<BusinessHours>
+    {
+        public BusinessHoursRepository(ApplicationDbContext db) : base(db)
+        {
+
+        }
+
+        public IQueryable<BusinessHours> GetHoursByBarId (int barId)
+        {
+            return _db.BusinessHours.Where(bh => bh.BarId == barId);
+        }
+    }
+}
---
+++ Barometr/src/Barometr/Models/BusinessHours.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Barometr.Models
+{
+    public class BusinessHours
+    {
+        public int Id { get; set; }
+        public string OpenTime { get; set; }
+        public string CloseTime { get; set; }
+        public int Day { get; set; }
+
+        [ForeignKey("BarId")]
+        public int BarId { get; set; }
+        public Bar Bar { get; set; }
+    }
+}
